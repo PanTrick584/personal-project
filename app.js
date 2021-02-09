394e191c318ab6556fbc4423febecc4110ae385f
PanTrick584 <53709752+PanTrick584@users.noreply.github.com>
2021-02-09 20:41:36 +0100

    evolution of layout is going

--- app.js
+++ app.js
@@ -15,72 +15,112 @@ const projectsBoxDOM = document.querySelectorAll('.projects__box')
 // SECTION CONTACT
 const contactDOM = document.querySelector('#contact');
 const contactHeaderDOM = document.querySelector('#contactHeader')
+// FILL
+const fillDOM = document.querySelectorAll('.fill');
+const reloadBackgroundsDOM = document.querySelector('#reloadBackgrounds')
 
-// FUNCTIONS
+let colorsArr = [
+    '#555555',
+    '#818181',
+    '#2c2c2c',
+    '#086972',
+    '#4997a0',
+    '#003e47',
+    '#ff9671',
+    '#ffc8a0',
+    '#c86745',
+    '#84ffa3',
+    '#b9ffd5',
+    '#4ecb73',
+    '#ffaad9',
+    '#ffdcff',
+    '#cb7aa7',
+    '#ffec7d',
+    '#ffba4c',
+    '#c88a17',
+    '#b38dff',
+    '#e7beff',
+    '#805fcb',
+    '#19fbff',
+    '#75ffff',
+    '#00c7cc'
+]
 
-//  setTimeout(()=> nameDOM.style.justifyContent = 'flex-start', 2000)
-
-//nav appear after click
-const navMenuDisplay = () => {
-    navBoxDOM.addEventListener("click", ()=> {
-        if(navMenuDOM.classList.contains("nav__menu-off")){
-
-            navMenuDOM.classList.remove("nav__menu-off");
-            navMenuDOM.classList.add("nav__menu-on");
-
-            //nav box rotate
-            navBoxDOM.classList.remove("nav__box-off");
-            navBoxDOM.classList.add("nav__box-on");
-        }
-        else if(navMenuDOM.classList.contains("nav__menu-on")){
-
-            navMenuDOM.classList.remove("nav__menu-on");
-            navMenuDOM.classList.add("nav__menu-off");
-
-            //nav box rotate
-            navBoxDOM.classList.remove("nav__box-on");
-            navBoxDOM.classList.add("nav__box-off");
-        }
-    })
+const changeBackground = () => {
+    colorsArrPrev = colorsArr;
+    if(colorsArr.length !== 0) {
+        fillDOM.forEach( fill => {
+            let color = colorsArr[Math.floor(Math.random()*colorsArr.length)]
+            fill.style.backgroundColor = color;
+            let newColorsArr = colorsArr.filter( col => col !== color );
+            colorsArr = newColorsArr
+            console.log(colorsArr, color)
+        })
+    }
+    colorsArr = colorsArrPrev;
 }
+reloadBackgroundsDOM.addEventListener('click', changeBackground)
+// //nav appear after click
+// const navMenuDisplay = () => {
+//     navBoxDOM.addEventListener("click", ()=> {
+//         if(navMenuDOM.classList.contains("nav__menu-off")){
 
-const slideHeader = (header) => {
+//             navMenuDOM.classList.remove("nav__menu-off");
+//             navMenuDOM.classList.add("nav__menu-on");
 
-    setTimeout( ()=>{
-        header.classList.remove("off__right");
-        header.classList.add("on__right");
-    }, 200);
-} 
-const slideParagraph = (paragraph) => {
+//             //nav box rotate
+//             navBoxDOM.classList.remove("nav__box-off");
+//             navBoxDOM.classList.add("nav__box-on");
+//         }
+//         else if(navMenuDOM.classList.contains("nav__menu-on")){
+
+//             navMenuDOM.classList.remove("nav__menu-on");
+//             navMenuDOM.classList.add("nav__menu-off");
+
+//             //nav box rotate
+//             navBoxDOM.classList.remove("nav__box-on");
+//             navBoxDOM.classList.add("nav__box-off");
+//         }
+//     })
+// }
+
+// const slideHeader = (header) => {
+
+//     setTimeout( ()=>{
+//         header.classList.remove("off__right");
+//         header.classList.add("on__right");
+//     }, 200);
+// } 
+// const slideParagraph = (paragraph) => {
     
-    setTimeout( ()=>{
-        paragraph.classList.remove("off__right");
-        paragraph.classList.add("on__right");
-    }, 400);
-} 
+//     setTimeout( ()=>{
+//         paragraph.classList.remove("off__right");
+//         paragraph.classList.add("on__right");
+//     }, 400);
+// } 
 
-// check if in veiwport
-function checkElementPosition(element) {
+// // check if in veiwport
+// function checkElementPosition(element) {
 
-    //Get element properties
-    let rect = element.getBoundingClientRect();
-    // GET SCREEN HEIGHT
-    let wHeight = document.documentElement.clientHeight;
-    //Check if in view    
-    let isVisible = (rect.top <= wHeight);
-    //Return 
-    return isVisible;
-  }
+//     //Get element properties
+//     let rect = element.getBoundingClientRect();
+//     // GET SCREEN HEIGHT
+//     let wHeight = document.documentElement.clientHeight;
+//     //Check if in view    
+//     let isVisible = (rect.top <= wHeight);
+//     //Return 
+//     return isVisible;
+//   }
 
-const checkViewport = () => {
-    findHeaderDOM.forEach( header => {
-        checkElementPosition( header) ? slideHeader( header ) : "";
-    })
-    findParagraphDOM.forEach( paragraph => {
-        checkElementPosition( paragraph ) ? slideParagraph( paragraph ) : "";
-    })
+// const checkViewport = () => {
+//     findHeaderDOM.forEach( header => {
+//         checkElementPosition( header) ? slideHeader( header ) : "";
+//     })
+//     findParagraphDOM.forEach( paragraph => {
+//         checkElementPosition( paragraph ) ? slideParagraph( paragraph ) : "";
+//     })
     
-}
+// }
 // EVENTS
 // SECTION NAME
 nameDOM.addEventListener( 'click', () => {
@@ -92,11 +132,6 @@ nameDOM.addEventListener( 'click', () => {
         nameDOM.classList.add('index-huge');
         nameDOM.classList.add('color-blue');
         nameHeaderDOM.classList.add('color-blue');
-        // Array.from( nameBoxDOM, box => box.classList.remove('off'))
-        // Array.from( nameBoxDOM, box => box.classList.add('on'))
-        
-        // nameDOM.classList.add('name__grid')
-        // setTimeout(()=> nameDOM.style.display = 'none', 300)
         
     } else {
         nameDOM.classList.remove('left-move');
@@ -105,9 +140,6 @@ nameDOM.addEventListener( 'click', () => {
         nameDOM.classList.add('index-small');
         nameDOM.classList.remove('color-blue');
         nameHeaderDOM.classList.remove('color-blue');
-        // Array.from( nameBoxDOM, box => box.classList.remove('on'))
-        // Array.from( nameBoxDOM, box => box.classList.add('off'))
-        // nameDOM.classList.remove('name__grid')
     }
 } )
 // SECTION PROJECTS
@@ -119,11 +151,6 @@ projectsDOM.addEventListener( 'click', () => {
         projectsDOM.classList.add('index-huge');
         projectsDOM.classList.add('color-green');
         projectsHeaderDOM.classList.add('color-green');
-
-        // Array.from( projectsBoxDOM, box => box.classList.remove('off'))
-        // Array.from( projectsBoxDOM, box => box.classList.add('on'))
-        
-        // projectsDOM.classList.add('projects__grid')
     } else {
         projectsDOM.classList.remove('bottom-move');
         projectsDOM.classList.add('bottom');
@@ -131,9 +158,6 @@ projectsDOM.addEventListener( 'click', () => {
         projectsDOM.classList.add('index-small');
         projectsDOM.classList.remove('color-green');
         projectsHeaderDOM.classList.remove('color-green');
-        // Array.from( projectsBoxDOM, box => box.classList.remove('on'))
-        // Array.from( projectsBoxDOM, box => box.classList.add('off'))
-        // projectsDOM.classList.remove('projects__grid')
     }
 } )
 // SECTION STORY
@@ -144,21 +168,14 @@ storyDOM.addEventListener( 'click', () => {
         storyDOM.classList.remove('index-small');
         storyDOM.classList.add('index-huge')
         storyDOM.classList.add('color-rose')
-        storyHeaderDOM.classList.add('color-rose')
-        // Array.from( storyBoxDOM, box => box.classList.remove('off'))
-        // Array.from( storyBoxDOM, box => box.classList.add('on'))
-        
-        // storyDOM.classList.add('story__grid')
+        storyHeaderDOM.classList.add('color-rose');
     } else {
         storyDOM.classList.remove('top-move');
         storyDOM.classList.add('top');
         storyDOM.classList.remove('index-huge');
         storyDOM.classList.add('index-small');
         storyDOM.classList.remove('color-rose')
-        storyHeaderDOM.classList.remove('color-rose')
-        // Array.from( storyBoxDOM, box => box.classList.remove('on'))
-        // Array.from( storyBoxDOM, box => box.classList.add('off'))
-        // storyDOM.classList.remove('story__grid')
+        storyHeaderDOM.classList.remove('color-rose');
     }
 } )
 // SECTION STORY
@@ -169,22 +186,14 @@ contactDOM.addEventListener( 'click', () => {
         contactDOM.classList.remove('index-small');
         contactDOM.classList.add('index-huge')
         contactDOM.classList.add('color-orange')
-        contactHeaderDOM.classList.add('color-orange')
-
-        // Array.from( storyBoxDOM, box => box.classList.remove('off'))
-        // Array.from( storyBoxDOM, box => box.classList.add('on'))
-        
-        // storyDOM.classList.add('story__grid')
+        contactHeaderDOM.classList.add('color-orange');
     } else {
         contactDOM.classList.remove('right-move');
         contactDOM.classList.add('right');
         contactDOM.classList.remove('index-huge');
         contactDOM.classList.add('index-small');
         contactDOM.classList.remove('color-orange')
-        contactHeaderDOM.classList.remove('color-orange')
-        // Array.from( storyBoxDOM, box => box.classList.remove('on'))
-        // Array.from( storyBoxDOM, box => box.classList.add('off'))
-        // storyDOM.classList.remove('story__grid')
+        contactHeaderDOM.classList.remove('color-orange');
     }
 } )
 // IMPORTANT MAY BE USEFULL LATER
@@ -201,5 +210,6 @@ contactDOM.addEventListener( 'click', () => {
 
 // window onload
 addEventListener("DOMContentLoaded", () => {
-    
+    changeBackground()
 });
+
